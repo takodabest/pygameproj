b919900d517f810f849fa97ad57775dde97bc9ef
NyquilEnthusiast <120580535+takodabest@users.noreply.github.com>
2023-05-03 10:18:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,18 @@ from random import randint, choice
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 
-while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      exit()
-      
-pygame.display.update()
-
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
-		player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-		player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+		player_walk_1 = pygame.image.load('images/walk1.png').convert_alpha()
+		player_walk_2 = pygame.image.load('images/walk2.png').convert_alpha()
 		self.player_walk = [player_walk_1,player_walk_2]
 		self.player_index = 0
-		self.player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+		self.player_jump = pygame.image.load('images/jump.png').convert_alpha()
 
 		self.image = self.player_walk[self.player_index]
 		self.rect = self.image.get_rect(midbottom = (80,300))
 		self.gravity = 0
-
-		self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
-		self.jump_sound.set_volume(0.5)
 
 	def player_input(self):
 		keys = pygame.key.get_pressed()
@@ -55,3 +44,12 @@ class Player(pygame.sprite.Sprite):
 		self.animation_state()
 		
   
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      exit()
+      
+pygame.display.update()
+
